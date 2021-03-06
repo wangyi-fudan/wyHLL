be95d98d8f29e5ab0e8ca58a73e0129f698d14a4
王一 WangYi <godspeed_china@yeah.net>
2021-03-06 18:02:17 +0800

    Add files via upload

--- bench.cpp
+++ bench.cpp
@@ -6,18 +6,21 @@
 #include	<vector>
 using	namespace	std;
 const	size_t	N=0x10000;
+const	size_t	bits=4;
 
 int	main(void){
-	uint64_t	seed=0;	double	rmse0=0,	rmse1=0;
+	uint64_t	seed=0,	r;
+	uint8_t	*s;	wyset_alloc(&s,bits);	wyhll<bits>	t;
+	double	rmse0=0,	rmse1=0;
 	for(size_t	it=0;	it<N;	it++){
 		uint64_t	n=pow(2,wy2u01(wyrand(&seed))*16);
-		wyhll<14> as;
-		for(size_t	i=0;	i<n;	i++){	uint64_t	r=wyrand(&seed);	as.add(&r,8);	}
-		double	x=log(as.estimate());
-		double	y=log(as.old_estimate());
-		double	z=log(n);
-		rmse0+=(x-z)*(x-z);
-		rmse1+=(y-z)*(y-z);
+		wyset_clear(s,bits);	t.clear();
+		for(size_t	i=0;	i<n;	i++){	r=wyrand(&seed);	wyset_add(s,bits,&r,8);	t.add(&r,8);	}
+		double	x=wyset_estimator(s,bits);
+		double	y=t.estimate();
+		double	z=n;
+		x=log(x),	y=log(y);	z=log(z);
+		rmse0+=(x-z)*(x-z);	rmse1+=(y-z)*(y-z);
 	}
 	cerr<<sqrt(rmse0/N)<<'\t'<<sqrt(rmse1/N)<<'\t'<<rmse0/rmse1<<'\n';
 	return	0;
