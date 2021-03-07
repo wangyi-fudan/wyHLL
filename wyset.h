292085f3faabe20730f0aa11869c58375297ec1c
王一 WangYi <godspeed_china@yeah.net>
2021-03-07 16:01:51 +0800

    Add files via upload

--- wyset.h
+++ wyset.h
@@ -3,6 +3,8 @@
 #define	wyset_version_0
 #include	"wyhash.h"
 #include	<math.h>
+#include	<iostream>
+using	namespace	std;
 struct	wyset{	uint8_t	*data,	bits,	layer;	};
 static	inline	void	wyset_alloc(wyset	*s,	uint8_t	bits,	uint8_t	layer){	s->bits=bits;	s->layer=layer;	s->data=(uint8_t*)calloc(((uint64_t)s->layer)<<s->bits,1);	}
 static	inline	void	wyset_free(wyset	*s){	free(s->data);	}
@@ -13,31 +15,30 @@ static	inline	void	wyset_add(wyset	*s,	void	*item,	uint64_t	item_size){
 	h&=(8ull<<s->bits)-1;
 	s->data[(lz<<s->bits)+(h>>3)]|=1u<<(h&7);
 }
-static	inline	double	wyset_loglike(wyset	*s,	uint64_t	*m,	double	N){
-	uint64_t	n=8ull<<s->bits;	double	lk=0;
-	N=exp(N)/n;
-	for(uint8_t	l=0;	l<s->layer;	l++){
-		double	lambda=l==s->layer-1?N/(1ull<<l):N/(1ull<<l)*0.5;
-		lk+=-lambda*(n-m[l])+m[l]*log(1-exp(-lambda));
-	}
-	return	lk;
-}
 static	inline	double	wyset_solve(wyset	*s,	uint64_t	*m){
-	const	double	gr=(sqrt(5.0)+1)/2;
-	double	a=log(1.0/16),	b=log(~0ull),	c=b-(b-a)/gr,	d=a+(b-a)/gr;
- 	while(fabs(b-a)>1e-4){
-        if(wyset_loglike(s,m,c)>wyset_loglike(s,m,d))	 b=d;	else	a=c;
-        c=b-(b-a)/gr;	d=a+(b-a)/gr;
+	double	n=8ull<<s->bits;
+	double	p[64]={},	N=(s->bits+s->layer+2)*M_LN2;
+	for(uint8_t	l=0;	l<s->layer;	l++)	p[l]=l==s->layer-1?1.0/n/(1ull<<l):1.0/n/(1ull<<l)/2;
+	for(size_t	it=0;	it<100;	it++){	
+		double	dn=0,	dnn=0,	en=exp(N);
+		for(uint8_t	l=0;	l<s->layer;	l++){	
+			double	enp=exp(-en*p[l]);
+			dn+=-en*p[l]*(n-m[l])+m[l]*en*p[l]*enp/(1-enp);
+			dnn+=-en*p[l]/(1-enp)/(1-enp)*(n*enp*enp+(m[l]-2*n+en*p[l]*m[l])*enp+n-m[l]);
+		}
+		N-=dn/dnn;
+		if(fabs(dn/dnn)<1e-6)	break;
 	}
-    return exp((b+a)/2);
+	return	exp(N);
 }
+
 static	inline	double	wyset_estimator(wyset	*s){
-	uint64_t	m[64]={};
+	uint64_t	m[64]={};	bool	empty=true;
 	for(uint64_t	l=0;	l<s->layer;	l++){
 		uint64_t	sum=0;	uint8_t	*p=s->data+(l<<s->bits);
 		for (uint64_t	i=0;	i<(1ull<<s->bits);	i+=8)	sum+=__builtin_popcountll(*(uint64_t*)(p+i));
-		m[l]=sum;
+		m[l]=sum;	if(m[l]!=(8ull<<s->bits))	empty=false;
 	}
-	return	wyset_solve(s,	m);
+	return	empty?0:wyset_solve(s,	m);
 }
 #endif
