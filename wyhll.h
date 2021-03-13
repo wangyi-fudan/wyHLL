836a5b22816d551c486ecae6b60fe5b155145dd1
王一 WangYi <godspeed_china@yeah.net>
2021-03-13 12:51:38 +0800

    Add files via upload

--- wyhll.h
+++ wyhll.h
@@ -21,45 +21,62 @@
 
 typedef  struct  wyhll_t{  
   uint64_t bins;
-  uint32_t p0, p1; 
+  uint32_t p[6]; 
 }wyhll;
 
 static  inline  void  wyhll_configure(wyhll  *s,  uint64_t  bytes,  uint64_t  capacity){  
-  s->bins=bytes<<2;
+  s->bins=(bytes-1)*8/3;
   double p=(double)s->bins/capacity;
   if(p>0.25) p=0.25;
-  s->p0=p*(1ull<<32);
-  s->p1=sqrt(p)*(1ull<<32);
+  for(uint32_t	i=0;	i<6;	i++) s->p[i]=pow(p,(i+1)/6.0)*(1ull<<32);
 }
 
-static  inline  uint64_t  wyhll_alloc(wyhll  s,  uint8_t  **data){  *data=(uint8_t*)calloc(s.bins>>2,1);  return  data!=NULL; }
+static  inline  uint64_t  wyhll_bytes(wyhll  s){  return s.bins*3/8+1; }
 
-static  inline  void  wyhll_clear(wyhll  s,  uint8_t  *data){  memset(data,0,s.bins>>2);  }
+static  inline  uint64_t  wyhll_alloc(wyhll  s,  uint8_t  **data){  *data=(uint8_t*)calloc(wyhll_bytes(s),1);  return  data!=NULL; }
 
-static  inline  uint8_t wyhll_get(uint8_t *data, uint64_t i){ return (data[i>>2]>>((i&3)<<1))&3; }
+static  inline  void  wyhll_clear(wyhll  s,  uint8_t  *data){  memset(data,0,wyhll_bytes(s));  }
 
-static  inline  void wyhll_set(uint8_t *data, uint64_t i, uint8_t v){ data[i>>2]&=~(3<<((i&3)<<1)); data[i>>2]|=v<<((i&3)<<1); }
+static  inline  uint8_t wyhll_get(uint8_t *data, uint64_t i){ 
+  uint64_t b=(i*3)>>3, f=(i*3)&7,f8=8-f;
+  return ((data[b]>>f)|(data[b+1]<<f8))&7;
+}
+
+static  inline  void wyhll_set(uint8_t *data, uint64_t i, uint8_t v){  
+  uint64_t b=(i*3)>>3, f=(i*3)&7,f8=8-f;
+  data[b]&=~(7<<f); data[b]|=v<<f; data[b+1]&=~(7>>f8); data[b+1]|=v>>f8;
+}
 
 static  inline  void  wyhll_add(wyhll  s,  uint8_t  *data,  void  *item,  uint64_t  item_size){
   uint64_t h=wyhash(item,item_size,0,_wyp);
   uint64_t b=((h>>32)*s.bins)>>32;
-  uint8_t o;
-  if((uint32_t)h>=s.p1) o=1;  else if((uint32_t)h>=s.p0) o=2; else o=3;
-  if(o>wyhll_get(data,b)) wyhll_set(data,b,o);
+  uint8_t v; h=(uint32_t)h;
+  if(h>=s.p[0]) v=1;
+  else if(h>=s.p[1]) v=2;
+  else if(h>=s.p[2]) v=3;
+  else if(h>=s.p[3]) v=4;
+  else if(h>=s.p[4]) v=5;
+  else if(h>=s.p[5]) v=6;
+  else v=7;
+  if(v>wyhll_get(data,b)) wyhll_set(data,b,v);
 }
 
 static  inline  double  wyhll_loglike(wyhll  s,  uint32_t  *m,  double  N){
-  double sum=0, p0=(double)s.p0/(1ull<<32), p1=(double)s.p1/(1ull<<32);
-  sum+=m[3]*log1p(-exp(-p0*N/s.bins));
-  sum+=m[2]*log(exp(-p0*N/s.bins)-exp(-p1*N/s.bins));
-  sum+=m[1]*log(exp(-p1*N/s.bins)-exp(-N/s.bins));
+  double sum=0;
   sum-=m[0]*N/s.bins;
+  sum+=m[1]*log(exp(-N/s.bins*s.p[0]/(1ull<<32))-exp(-N/s.bins));
+  sum+=m[2]*log(exp(-N/s.bins*s.p[1]/(1ull<<32))-exp(-N/s.bins*s.p[0]/(1ull<<32)));
+  sum+=m[3]*log(exp(-N/s.bins*s.p[2]/(1ull<<32))-exp(-N/s.bins*s.p[1]/(1ull<<32)));
+  sum+=m[4]*log(exp(-N/s.bins*s.p[3]/(1ull<<32))-exp(-N/s.bins*s.p[2]/(1ull<<32)));
+  sum+=m[5]*log(exp(-N/s.bins*s.p[4]/(1ull<<32))-exp(-N/s.bins*s.p[3]/(1ull<<32)));
+  sum+=m[6]*log(exp(-N/s.bins*s.p[5]/(1ull<<32))-exp(-N/s.bins*s.p[4]/(1ull<<32)));
+  sum+=m[7]*log1p(-exp(-N/s.bins*s.p[5]/(1ull<<32)));
   return sum;
 }
 
 static  inline  uint64_t  wyhll_solve(wyhll  s, uint32_t  *m){
   if(m[0]==s.bins)  return  0;
-  double  r=(sqrt(5.0)+1)/2, a=0, b=log(s.bins<<32)-log(s.p0), c=b-(b-a)/r, d=a+(b-a)/r;
+  double  r=(sqrt(5.0)+1)/2, a=0, b=log(s.bins<<32)-log(s.p[5]), c=b-(b-a)/r, d=a+(b-a)/r;
   while(fabs(a-b)>1e-5){
     if(wyhll_loglike(s,m,exp(c))>wyhll_loglike(s,m,exp(d)))   b=d;  
     else  a=c;
@@ -69,13 +86,13 @@ static  inline  uint64_t  wyhll_solve(wyhll  s, uint32_t  *m){
 }
 
 static  inline  uint64_t  wyhll_cardinality(wyhll  s, uint8_t *data){
-  uint32_t m[4]={};
+  uint32_t m[8]={};
   for(uint64_t  i=0; i<s.bins; i++)  m[wyhll_get(data,i)]++;
   return wyhll_solve(s,m);
 }
 
 static  inline  uint64_t  wyhll_union_cardinality(wyhll  s, uint8_t *data_a, uint8_t *data_b){
-  uint32_t m[4]={};
+  uint32_t m[8]={};
   for(uint64_t  i=0; i<s.bins; i++){
     uint8_t va=wyhll_get(data_a,i), vb=wyhll_get(data_b,i);
   	m[va>vb?va:vb]++;
